6faedd5d0953481707e29c18fbfc3771b79cabce
Maksim Amelchenko <m.amelchenko@gmail.com>
2023-11-18 22:00:16 +0100

    chore: set up shadcn ui

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
+import { Button } from '@org/ui-kit';
+
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="frontend" />
+      <Button variant="destructive">Button</Button>
     </div>
   );
 }
